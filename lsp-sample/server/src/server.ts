8ef010c487078ed9bb6bf8d54093a8ee4c113aa1
williammleziva <willmlez@gmail.com>
2023-03-21 18:03:43 -0500

    remove old code, tranfer to other file working

--- lsp-sample/server/src/server.ts
+++ lsp-sample/server/src/server.ts
@@ -248,107 +248,11 @@ connection.onDidChangeWatchedFiles(_change => {
 	connection.console.log('We received an file change event');
 });
 
-// This handler provides the initial list of the completion items.
-// connection.onCompletion(
-// 	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-// 		// The pass parameter contains the position of the text document in
-// 		// which code complete got requested. For the example we ignore this
-// 		// info and always provide the same completion items.
-// 	//console.log("onCompletion called");
-// 		return [
-// 			{
-// 				label: 'title',
-// 				kind: CompletionItemKind.Text,
-// 				data: 1
-// 			},
-// 			{
-// 				label: 'description',
-// 				kind: CompletionItemKind.Text,
-// 				data: 2
-// 			}
-// 		];
-// 	}
-// );
-
-
-// connection.onCompletion(
-// 	(textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-// 		const lineNumber = textDocumentPosition.position.line;
-// 		const document = documents.get(textDocumentPosition.textDocument.uri);
-// 		const lineText = document?.getText({
-// 			start: { line: lineNumber, character: 0 },
-// 			end: { line: lineNumber, character: Number.MAX_VALUE }
-// 		});
-
-// 		if (lineText?.includes('title:') || lineText?.includes('description:')) {
-// 			return [];
-// 		}
-
-// 		// Return the completion items if the current line does not contain one yet
-// 		return [
-// 			{
-// 				label: 'title:',
-// 				kind: CompletionItemKind.Text,
-// 				data: 1
-// 			},
-// 			{
-// 				label: 'description:',
-// 				kind: CompletionItemKind.Text,
-// 				data: 2
-// 			}
-// 		];
-// 	}
-// );
 connection.onCompletion((textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-        // Get the current line number and text of the document being edited
-        // const lineNumber = textDocumentPosition.position.line;
-        // const document = documents.get(textDocumentPosition.textDocument.uri);
-        // const lineText = document?.getText({
-        //     start: { line: lineNumber, character: 0 },
-        //     end: { line: lineNumber, character: Number.MAX_VALUE }
-        // });
 
-		// if (!document) {
-		// 	return [];
-		// }
+	return handleCompletion(documents,textDocumentPosition);
 
-		return handleCompletion(documents,textDocumentPosition);
-
-        // Get a list of words that have already been written in the current text document
-    //     const existingWords = new Set<string>();
-    //     for (let i = 0; i < document!.lineCount; i++) {
-    //         const line = document!.getText({
-    //             start: { line: i, character: 0 },
-    //             end: { line: i, character: Number.MAX_VALUE }
-    //         });
-    //         const words = line.trim().split(/\s+/);
-    //         for (let word of words) {
-	// 			existingWords.add(word);
-	// 		  }
-    //     }
-
-
-    //     const filteredItems = [
-    //         {
-    //             label: 'title:',
-    //             kind: CompletionItemKind.Text,
-    //             data: 1
-    //         },
-    //         {
-    //             label: 'description:',
-    //             kind: CompletionItemKind.Text,
-    //             data: 2
-    //         }
-    //     ].filter(item => !existingWords.has(item.label));
-
-    //     // Return the filtered completion items if the current line does not contain one yet
-    //     if (!lineText?.includes('title') && !lineText?.includes('description')) {
-    //         return filteredItems;
-    //     } else {
-    //         return [];
-    //     }
-    // }
-);
+});
 
 
 
