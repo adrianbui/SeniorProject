e18b6a91c5eef0f3d1d1ef81e2e31f58f2a2024c
wstaib <wstaib94@yahoo.com>
2023-04-07 17:46:53 -0500

    debugging checkForWrongKeys

--- sigma-lsp/server/src/diagnostics.ts
+++ sigma-lsp/server/src/diagnostics.ts
@@ -12,8 +12,7 @@ import {
 import { error } from 'console';
 import { getTags } from 'yaml/dist/schema/tags';
 import {requiredTypes} from './sigmaRequiredTypes';
-import { fieldOrder } from './sigmaFieldOrder';
-import { subFieldOrderLogSource } from './sigmaFieldOrder';
+import { fieldOrder, subFieldOrderLogSource } from './sigmaFieldOrder';
 
 
 // Code adapted from https://github.com/humpalum/vscode-sigma/blob/main/src/diagnostics.ts
@@ -31,12 +30,12 @@ export function handleDiagnostics(doc: TextDocument, parsedToJS: Record<string, 
 	// flatten parsedToJS so that nested keys exist at the top level
 	// this helps loop through all keys in checkType
 	const flatParsedToJS = flattenObject(parsedToJS);
-	console.log('flattened parsedToJS', flatParsedToJS);
+	//console.log('flattened parsedToJS', flatParsedToJS);
 	// call checkType on all keys including nested keys
 	diagnostics.push(...checkTypeOfAllKeys(flatParsedToJS, doc,lines));
 
 	if("tags" in parsedToJS) {
-		console.log('tags attribute exists');
+		//console.log('tags attribute exists');
 		const tempArr = checkLowercaseTags(doc, lines, parsedToJS);
 		//const tempArr2 = checkType("array", "tags", doc, lines, parsedToJS);
 		diagnostics.push(...tempArr);
@@ -138,19 +137,27 @@ function flattenObject(parsedToJS:Record<string, unknown>){
  */
 function checkFieldOrder(parsedToJS:Record<string, unknown>){
 	const ordering:any = {}; // map for efficient lookup of sortIndex
-    const sortOrder = fieldOrder;
+    const sortOrder = fieldOrder; //fieldOrder is the correct order according to sigma docs
+	//console.log("sortOrder: ", sortOrder);
 	const keysArr = Object.keys(parsedToJS);
+	// create a copy of keysArr to avoid mutating when sorting
+	const sortedKeysArr = [...keysArr]; // this array will be sorted, representing the correct ordering
 	for (let i=0; i<sortOrder.length; i++){
 		ordering[sortOrder[i]] = i;
 	}
-	const sortedKeysArr = keysArr.sort(function(a, b) {
+	//console.log("ordering: ", ordering);
+	sortedKeysArr.sort(function(a, b) {
 		return (ordering[a] - ordering[b]);
 	});
+	console.log("keysArr: ", keysArr);
+	console.log("sortedKeysArr: ", sortedKeysArr);
 	const wrongKeys = []; 
 	for(let i=0; i<keysArr.length; i++){ 
-		const key = keysArr[i];
-		if(sortedKeysArr[i]!=key){
-			wrongKeys.push(key);
+		const actualKey = keysArr[i];
+		const correctKey = sortedKeysArr[i];
+		console.log(`actual key: ${actualKey}, correct key: ${correctKey}`);
+		if(actualKey != correctKey){
+			wrongKeys.push(actualKey);
 		}
 	}
 	return wrongKeys;
@@ -166,17 +173,34 @@ function checkFieldOrder(parsedToJS:Record<string, unknown>){
 function checkForWrongKeys(doc: TextDocument, docLines: Array<string>,parsedToJS:Record<string,unknown>){
 	const diagnostics: Diagnostic[] = [];
 	const wrongKeysArr = checkFieldOrder(parsedToJS);
+	const numWrongKeys = wrongKeysArr.length;
+	if (numWrongKeys === 0) { // no fields are in the wrong order
+		return diagnostics;
+	}
+	console.log("wrongKeysArr: ", wrongKeysArr);
 	let count = 0; // for looping through wrongKeysArr
 	let currentKey = wrongKeysArr[count];
 	let regex = new RegExp(`^\\s*${currentKey}:`);
 	for(let i=0; i<docLines.length; i++){
+		console.log("currentKey: ", currentKey);
+		console.log("regex: ", regex);
 		const regexMatchIndex = docLines[i].search(regex); // first character of the matched key
-		if(regexMatchIndex > 0){
+		console.log("regexMatchIndex", regexMatchIndex);
+		if(regexMatchIndex >= 0){
+			console.log("found regex match");
 			const lineNumber = i;
 			diagnostics.push(createDiaFieldOutOfOrder(doc,currentKey,lineNumber,regexMatchIndex));
-			count++;
-			currentKey = wrongKeysArr[count];
-			regex = new RegExp(`^\\s*${currentKey}:`);
+			
+			if (count >= numWrongKeys - 1){   // we've found all the incorrect keys
+				return diagnostics;
+			} else { // look for the next incorrect key
+				console.log("current count:", count);
+				console.log("numWrongKeys :", numWrongKeys);
+				count++;
+				currentKey = wrongKeysArr[count];
+				regex = new RegExp(`^\\s*${currentKey}:`);
+			}
+			
 		}
 	}
 	return diagnostics;
@@ -404,7 +428,7 @@ function checkType(
 	): [Diagnostic[], number]{
 	const tempDiagnostics: Diagnostic[] = [];
 	const sigmaValue = parsedToJS[sigmaKey]; // parsed value for the current sigma key
-	console.log(`called checkType on key ${sigmaKey}, required type is: ${requiredType}`);
+	//console.log(`called checkType on key ${sigmaKey}, required type is: ${requiredType}`);
 	let wrongType;
 	
 	if (requiredType === "string") {
@@ -427,16 +451,16 @@ function checkType(
 		// get the line that sigmaKey is on
 		for (let i = lastCheckedLine; i < doc.lineCount; i++) {
 			const lineString = docLines[i];
-			console.log('current Line: ', lineString);
+			//console.log('current Line: ', lineString);
 			const regex = new RegExp(`^\\s*${sigmaKey}:`); // to find line line that starts with sigmaKey
-			console.log('regex', regex);
+			//console.log('regex', regex);
 			if (lineString.match(regex)) {
 				// get the next key so that the current diagnostic will be on all lines until the next key
 				
 				const keys = Object.keys(parsedToJS);
 				const nextIndex = keys.indexOf(sigmaKey)+1;
 				const nextField = keys[nextIndex];
-				console.log(`next field after ${sigmaKey}: `, nextField);
+				//console.log(`next field after ${sigmaKey}: `, nextField);
 
 				if (nextField){ // if sigmaKey isn't the last field
 					let j=i+1;
@@ -449,17 +473,17 @@ function checkType(
 						thisLine = docLines[j];
 						matchedRegex = thisLine.match(regex);
 						//console.log('matched Regex: ', matchedRegex);
-						console.log('thisLine: ', thisLine);
+						//console.log('thisLine: ', thisLine);
 						j++;
 					}
 					if (!matchedRegex){ // just in case we didn't find nextField for some reason
 						tempDiagnostics.push(createDiaIncorrectType(doc,lineString,i,i,sigmaKey,requiredType));
 					} else {
-						console.log('last line j was at: ', thisLine);
+						//console.log('last line j was at: ', thisLine);
 						const idxBeforeNextField = j-2;
 						lastCheckedLine = idxBeforeNextField; // to pass in the next time we call checkType
 						const lastString = docLines[idxBeforeNextField]; // the line before nextField is the last line in the diagnostic
-						console.log('last line in current diagnostic: ', lastString);
+						//console.log('last line in current diagnostic: ', lastString);
 						tempDiagnostics.push(createDiaIncorrectType(doc,lastString,i,idxBeforeNextField,sigmaKey,requiredType));
 					}
 				} else { // if current key is the last field
@@ -606,9 +630,9 @@ function checkLowercaseTags(doc: TextDocument, docLines: Array<string>, parsedTo
 					if (commentStart !== -1) {
 						lineString = lineString.substring(0,commentStart);
 					}
-					console.log(lineString);
+					//console.log(lineString);
 					const uppercaseWords = lineString.match(/\b[A-Z]\S*\b/g);
-					console.log(uppercaseWords);
+					//console.log(uppercaseWords);
 					if (uppercaseWords) {
 						for (let k=0; k<uppercaseWords.length; k++){
 							const badWord = uppercaseWords[k];
@@ -640,7 +664,7 @@ function checkAuthor(doc: TextDocument, docLines: Array<string>, parsedToJS: Rec
 	const tempDiagnostics: Diagnostic[] = [];
 	const authorValue = parsedToJS.author;
 
-	console.log('checkAuthor called');
+	//console.log('checkAuthor called');
 	// TODO add diagnostic for needing quotation marks around @ symbol in author value
 
 	// value is invalid if not a string
@@ -654,7 +678,7 @@ function checkAuthor(doc: TextDocument, docLines: Array<string>, parsedToJS: Rec
 				const keys = Object.keys(parsedToJS);
 				const nextIndex = keys.indexOf("author")+1;
 				const nextField = keys[nextIndex];
-				console.log('next field after author: ', nextField);
+				//console.log('next field after author: ', nextField);
 
 				if (nextField){ // if author isn't the last field
 					let j=i+1;
@@ -665,17 +689,17 @@ function checkAuthor(doc: TextDocument, docLines: Array<string>, parsedToJS: Rec
 					while (!matchedRegex && j<doc.lineCount){
 						lastCheckedLine = docLines[j];
 						matchedRegex = lastCheckedLine.match(regex);
-						console.log('matched Regex: ', matchedRegex);
-						console.log('lastCheckedLine: ', lastCheckedLine);
+						//console.log('matched Regex: ', matchedRegex);
+						//console.log('lastCheckedLine: ', lastCheckedLine);
 						j++;
 					}
 					if (!matchedRegex){ // just in case we didn't find nextField for some reason
 						tempDiagnostics.push(createDiaAuthorNotString(doc,lineString,i,i));
 					} else {
-						console.log('last line j was at: ', lastCheckedLine);
+						//console.log('last line j was at: ', lastCheckedLine);
 						const idxBeforeNextField = j-2;
 						const lastString = docLines[idxBeforeNextField]; // the line before nextField is the last line in the diagnostic
-						console.log('last line in author diagnostic: ', lastString);
+						//console.log('last line in author diagnostic: ', lastString);
 						tempDiagnostics.push(createDiaAuthorNotString(doc,lastString,i,idxBeforeNextField));
 					}
 				} else { // if author is for some reason the last field
