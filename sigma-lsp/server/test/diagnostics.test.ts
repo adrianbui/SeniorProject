845ff405eca2427f1f7977b8d214cb7e4db04dde
Adrian Bui <adrian.t.bui@gmail.com>
2023-04-10 22:45:29 -0500

    set up and added unit tests

---
+++ sigma-lsp/server/test/diagnostics.test.ts
@@ -0,0 +1,7 @@
+import 'jest';
+
+describe('example', () => {
+    it('adds two numbers together', () => {
+      expect(2 + 2).toBe(4);
+    });
+});
---
+++ sigma-lsp/server/test/hover.test.ts
@@ -0,0 +1,8 @@
+import 'jest';
+import { handleHover } from '../src/hover';
+
+describe('example', () => {
+    it('adds two numbers together', () => {
+      expect(2 + 2).toBe(4);
+    });
+});
